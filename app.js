34360c997175471d98cebb45df89abdc168a5b47
Cesar <cesar.villasenor.g@gmail.com>
2020-12-17 20:42:07 -0800

    first update.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
+var {createPreference} = require('./mercadopago');
+const bodyParser = require('body-parser')
 
 var app = express();
  
 app.engine('handlebars', exphbs());
+
+app.use(bodyParser.ur...lencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 app.set('view engine', 'handlebars');
 
 app.use(express.static('assets'));
@@ -17,6 +24,28 @@ app.get('/', function (req, res) {
 
 app.get('/detail', function (req, res) {
     res.render('detail', req.query);
+});...
+
+app.get('/detail', async function (req, res) {
+    let id = await createPreference(req.query)
+    .then(id=> id)
+    .catch(err=> err)
+    console.log(req.query)
+    res.render('detail', {...req.query, idmp: global.id});
+});
+app.get('/success', function (req, res) {
+    res.render('success', req.query);
+});
+app.get('/pending', function (req, res) {
+    res.render('pending', req.query);
+});
+app.get('/failure', function (req, res) {
+    res.render('failure', req.query);
+});
+
+app.post('/checkout', function (req, res) {
+    console.log( req.body)
+    res.status(200).json({ok:true});
 });
 
 app.listen(port);
---
+++ config.js
@@ -0,0 +1,3 @@
+const baseUrl = 'https://cesarv-mp-commerce-nodejs.herokuapp.com/'
+
+module.exports = { baseUrl }
